c9be7315ee331873b6f9326aefe920622f47c1ad
PetrachkovStepan <158708945+PetrachkovStepan@users.noreply.github.com>
2024-06-13 15:24:24 +0300

    Project Creation and dependencies
    Tailwind
    Redux
    Firebase
    Axios
    ReactRouter
    React

---
+++ navigator-task/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "bg-slate-800 h-screen w-screen flex justify-center items-center">
+        <div className = " text-[100px] text-white">Instalation complete</div>
+    </div>
+  );
+}
+
+export default App;
